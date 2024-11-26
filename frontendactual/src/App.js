486692624e7016ce78a6901ce0b2782bc24912da
akshimital <124424497+akshimital@users.noreply.github.com>
2024-11-27 03:42:39 +0530

    Update App.js

--- frontendactual/src/App.js
+++ frontendactual/src/App.js
@@ -1,21 +1,25 @@
+import { Layout } from 'antd';
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider, AuthContext } from './context/AuthContext';
-import { CartProvider } from './context/CartContext'; // Import CartProvider
-import Home from './components/Home';
-import Login from './pages/Login';
-import SignUp from './pages/Signup';
-import FoodMenu from './components/FoodMenu';
-import Profile from './components/Profile';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import AdminRoute from './AdminRoute.js';
 import About from './components/About';
-import Contact from './components/Contact';
+import AdminDashboard from './components/AdminDashboard';
+import Customers from './components/admins/Pages/Customers/index.js';
+import Inventory from './components/admins/Pages/Inventory';
+import Orders from './components/admins/Pages/Orders/index.js';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
+import Contact from './components/Contact';
+import FoodMenu from './components/FoodMenu';
+import Home from './components/Home';
 import OrderConfirmation from './components/OrderConfirmation';
+import Profile from './components/Profile';
+import { AuthContext, AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext'; // Import CartProvider
+import Login from './pages/Login';
+import SignUp from './pages/Signup';
 import ProtectedRoute from './ProtectedRoutes'; // Import ProtectedRoute
-import AdminDashboard from './components/AdminDashboard.js'; // Admin dashboard component
-import AdminRoute from './AdminRoutes.js'; // AdminRoute component to protect admin routes
-
+const {Sider,Content}=Layout;
 function App() {
   const { user, isLoggedIn } = useContext(AuthContext);
 
@@ -23,73 +27,54 @@ function App() {
     <AuthProvider>
       <CartProvider> {/* Wrap components needing access to CartContext */}
         <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/food-menu" element={<FoodMenu />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
 
-            {/* Protected Routes for normal users */}
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/cart"
-              element={
-                <ProtectedRoute>
-                  <Cart />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/checkout"
-              element={
-                <ProtectedRoute>
-                  <Checkout />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/order-confirmation"
-              element={
-                <ProtectedRoute>
-                  <OrderConfirmation />
-                </ProtectedRoute>
-              }
-            />
+          
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/signup" element={<SignUp />} />
+                  <Route path="/food-menu" element={<FoodMenu />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="/admin" element={<AdminDashboard />} />
+                  <Route path="/inventory" element={<Inventory />} />
+                  <Route path="/orders" element={<Orders />} />
+                  <Route path="/customers" element={<Customers />} />
 
-            {/* Admin Routes */}
-            {/* <Route
-              path="/admin-dashboard"
-              element={
-                isLoggedIn && user?.isAdmin ? (
-                  <AdminDashboard />
-                ) : (
-                  <Login /> // Redirect to login if not admin
-                )
-              }
-            /> */}
-            {/* Alternatively, use AdminRoute to handle redirection */}
-            <Route
-              path="/admin"
-              element={
-                <AdminRoute>
-                  <AdminDashboard />
-                </AdminRoute>
-              }
-            />
-          </Routes>
+                  {/* Protected Routes for normal users */}
+                  <Route
+                    path="/profile"
+                    element={<ProtectedRoute>
+                      <Profile />
+                    </ProtectedRoute>} />
+                  <Route
+                    path="/cart"
+                    element={<ProtectedRoute>
+                      <Cart />
+                    </ProtectedRoute>} />
+                  <Route
+                    path="/checkout"
+                    element={<ProtectedRoute>
+                      <Checkout />
+                    </ProtectedRoute>} />
+                  <Route
+                    path="/order-confirmation"
+                    element={<ProtectedRoute>
+                      <OrderConfirmation />
+                    </ProtectedRoute>} />
+
+
+                  <Route
+                    path="/admin"
+                    element={<AdminRoute>
+                      <AdminDashboard />
+                    </AdminRoute>} />
+                </Routes>
+             
         </Router>
       </CartProvider>
     </AuthProvider>
   );
-}
+};
 
 export default App;
