5c31214776b38fe4f5827cec1a77be49d59d86ee
Akshitripathi <126938167+Akshitripathi@users.noreply.github.com>
2024-11-26 13:15:41 +0530

    Update authRoutes.js

--- backendactual/routes/authRoutes.js
+++ backendactual/routes/authRoutes.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const { sendOtp, verifyOtp, signup } = require('../controllers/authControllers');
-
+const { sendOtp, verifyOtp, signup, verifyToken } = require('../controllers/authControllers');
+const authMiddleware= require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.post('/login', sendOtp);      
+router.post('/user/login', sendOtp);      
  
-router.post('/signup',signup);
+router.post('/user/signup',signup);
 
-router.post('/verifyUserLogin', verifyOtp);
+router.post('/user/verifyUserLogin', verifyOtp);
+
+router.get('/status', authMiddleware(), (req, res) => {
+  res.status(200).json({
+      isLoggedIn: true,
+      user: req.user, // User info attached by the authMiddleware
+  });
+});
+router.post('/auth/logout', (req, res) => {
+    // Since JWT is stateless, logout is handled client-side by removing the token
+    res.status(200).json({ message: 'Logout successful, please clear the token on the client side' });
+  });
 
 module.exports = router;
